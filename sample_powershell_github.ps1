01d635843a6810e413fc4e62858162abfdf48984
U-CORP\sd2 <sd2@Intuita350269.corp.intuit.net>
2018-07-01 18:49:59 -0700

    Initial Commit

--- sample_powershell_github.ps1
+++ sample_powershell_github.ps1
@@ -1,3 +1,3 @@
 param($variablename)
-wtite-host "My First Powershell Executed in Jenkins via github...$variablename"
+write-host "My First Powershell Executed in Jenkins via github...$variablename"
 Get-Service -servicename VMTools
