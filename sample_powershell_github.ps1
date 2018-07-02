3b28733be40332a4ba9f9824445cdc495f89e34b
U-CORP\sd2 <sd2@Intuita350269.corp.intuit.net>
2018-07-01 20:07:18 -0700

    Initial Commit

--- sample_powershell_github.ps1
+++ sample_powershell_github.ps1
@@ -1,3 +1,22 @@
-param($variablename)
+﻿param($variablename)
 write-host "My First Powershell Executed in Jenkins via github...$variablename"
-Get-Service -servicename VMTools
+
+
+function validateConnection {
+
+    param($compName)
+
+    if ( (Test-Connection -ComputerName $compName -Count 2 -Quiet) -eq "True" )
+    {
+        Write-Host "$compName is Online" -ForegroundColor Green
+    }
+    else {Write-Host "$compName is Offline" -ForegroundColor Red}
+
+}
+
+
+$hostnames = @("192.168.145.140", "192.168.145.145")
+
+ForEach ($hostname in $hostnames) {
+    validateConnection -compName $hostname
+}
